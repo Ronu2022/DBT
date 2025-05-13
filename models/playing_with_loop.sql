45610da1258b85060fa03411bf1ea935aa4a6af2
Ronu Mondeep Mohanty <pmohanty.mohanty7@gmail.com>
2025-05-13 04:51:14 +0000

    commit

--- models/playing_with_loop.sql
+++ models/playing_with_loop.sql
@@ -10,3 +10,17 @@ select
 FROM {{ source("payments_types_sources","customer_payments")}}
 group by id
 
+-- Getting the first of the Loop:
+
+{% set column_values = dbt_utils.get_column_values(
+    source("payments_types_sources","customer_payments"),"payment_mode"
+) %}
+select
+    id,
+    {% for col in column_values -%}
+    {%- if not loop.first -%},{%- endif %}
+    sum(case when payment_mode = '{{ col }}' then payment_value else 0 end) 
+    as {{ col | replace(' ','') | lower }}_payments
+    {% endfor -%}
+from {{ source("payments_types_sources","customer_payments")}}
+group by id
